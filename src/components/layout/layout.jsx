cdd4e45dd171f67f279b8db557f591fa94d9f871
Yogesh Rathod <yogesh.rathod@loylty.in>
2019-01-10 12:50:59 +0530

    --amend

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import RecentViews from '../common/recents/recents';
+import NewsLetter from '../common/newsletter/newsletter';
+
+const Layout = props => {
+    console.log('props.children ', props);
+    return (
+        <div>
+            <Header />
+            {props.children}
+            <RecentViews />
+            <NewsLetter />
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,17 +1,17 @@
-const API_URL = {
-    bestSeller: `http://localhost:5000/products/getTop20`,
-    banners: `http://localhost:5000/products/getBanners`,
-    recents: `http://localhost:5000/products/getRecents`,
-    categories: `http://localhost:5000/products/getCategories`,
-    loginUrl: `http://localhost:5000/users/`,
-    productsUrl: `http://localhost:5000/products/cart`
-};
-
 // const API_URL = {
-//     bestSeller: `https://api.myjson.com/bins/81wx8`,
-//     banners: `https://api.myjson.com/bins/rff9g`,
-//     recents: `https://api.myjson.com/bins/yags4`,
-//     categories: `https://api.myjson.com/bins/md7uk`
+//     bestSeller: `http://localhost:5000/products/getTop20`,
+//     banners: `http://localhost:5000/products/getBanners`,
+//     recents: `http://localhost:5000/products/getRecents`,
+//     categories: `http://localhost:5000/products/getCategories`,
+//     loginUrl: `http://localhost:5000/users/`,
+//     productsUrl: `http://localhost:5000/products/cart`
 // };
+
+const API_URL = {
+    bestSeller: `https://api.myjson.com/bins/81wx8`,
+    banners: `https://api.myjson.com/bins/rff9g`,
+    recents: `https://api.myjson.com/bins/yags4`,
+    categories: `https://api.myjson.com/bins/md7uk`
+};
 
 export default API_URL;
