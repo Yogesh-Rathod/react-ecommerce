51a3e2260db668433720a94ac53957b42f1f5a60
Yogesh Rathod <yogesh.rathod@loylty.in>
2019-01-18 16:23:20 +0530

    Sort Done

--- src/containers/products-listing/products-listing.jsx
+++ src/containers/products-listing/products-listing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
 
 import ShopItem from '../../components/shop-item/item';
 import API_URL from '../../environments/environment';
@@ -10,7 +11,11 @@ class ProductListing extends React.Component {
         this.state = {
             products: [],
             categories: [],
-            brands: []
+            brands: [],
+            sort: {
+                selected: 'highest rated',
+                keys: ['highest rated', 'name', 'price']
+            }
         };
     }
 
@@ -41,135 +46,194 @@ class ProductListing extends React.Component {
         });
     }
 
+    sortProducts(criteria) {
+        let selectedCriteria = { ...this.state.sort };
+        selectedCriteria.selected = criteria;
+        this.setState({ sort: selectedCriteria });
+        this.sortByPrice(criteria);
+    }
+
+    sortByPrice(criteria) {
+        let sortItem = '';
+        let isNameSort = false;
+        switch (criteria) {
+            case 'price':
+                sortItem = 'price';
+                break;
+            case 'highest rated':
+                sortItem = 'popularity';
+                break;
+            case 'name':
+                isNameSort = true;
+                sortItem = 'name';
+                break;
+            default:
+                break;
+        }
+        let sortedProducts = this.state.products.sort((a, b) => {
+            if (!isNameSort) {
+                return a[sortItem] - b[sortItem];
+            } else {
+                var nameA = a.name.toLowerCase(),
+                    nameB = b.name.toLowerCase();
+                if (nameA < nameB) return -1;
+                if (nameA > nameB) return 1;
+                return 0;
+            }
+        });
+        console.log('sortedProducts ', sortedProducts);
+        this.setState({
+            products: sortedProducts
+        });
+    }
+
     render() {
         return (
-            <div className="shop">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3">
-                            <div className="shop_sidebar">
-                                <div className="sidebar_section">
-                                    <div className="sidebar_title">
-                                        Categories
+            <div>
+                <Helmet>
+                    <title>Product Listing Page</title>
+                </Helmet>
+                <div className="shop">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <div className="shop_sidebar">
+                                    <div className="sidebar_section">
+                                        <div className="sidebar_title">
+                                            Categories
+                                        </div>
+                                        {this.state.categories &&
+                                        this.state.categories.length ? (
+                                            <ul className="sidebar_categories">
+                                                {this.state.categories.map(
+                                                    (item, index) => {
+                                                        return (
+                                                            <li
+                                                                className="brand"
+                                                                key={index}
+                                                            >
+                                                                <a href="javascript:void(0)">
+                                                                    {item}
+                                                                </a>
+                                                            </li>
+                                                        );
+                                                    }
+                                                )}
+                                            </ul>
+                                        ) : (
+                                            'No Categories!'
+                                        )}
                                     </div>
-                                    {this.state.categories &&
-                                    this.state.categories.length ? (
-                                        <ul className="sidebar_categories">
-                                            {this.state.categories.map(
-                                                (item, index) => {
-                                                    return (
-                                                        <li
-                                                            className="brand"
-                                                            key={index}
-                                                        >
-                                                            <a href="javascript:void(0)">
-                                                                {item}
-                                                            </a>
-                                                        </li>
-                                                    );
-                                                }
-                                            )}
-                                        </ul>
-                                    ) : (
-                                        'No Categories!'
-                                    )}
-                                </div>
-                                <div className="sidebar_section filter_by_section">
-                                    <div className="sidebar_title">
-                                        Filter By
-                                    </div>
-                                    <div className="sidebar_subtitle">
-                                        Price
-                                    </div>
-                                    <div className="filter_price">
-                                        <div
-                                            id="slider-range"
-                                            className="slider_range"
-                                        />
-                                        <p>Range: </p>
-                                        <p>
-                                            <input
-                                                type="text"
-                                                id="amount"
-                                                className="amount"
+                                    <div className="sidebar_section filter_by_section">
+                                        <div className="sidebar_title">
+                                            Filter By
+                                        </div>
+                                        <div className="sidebar_subtitle">
+                                            Price
+                                        </div>
+                                        <div className="filter_price">
+                                            <div
+                                                id="slider-range"
+                                                className="slider_range"
                                             />
-                                        </p>
+                                            <p>Range: </p>
+                                            <p>
+                                                <input
+                                                    type="text"
+                                                    id="amount"
+                                                    className="amount"
+                                                />
+                                            </p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="sidebar_section">
-                                    <div className="sidebar_subtitle brands_subtitle">
-                                        Brands
+                                    <div className="sidebar_section">
+                                        <div className="sidebar_subtitle brands_subtitle">
+                                            Brands
+                                        </div>
+                                        {this.state.brands &&
+                                        this.state.brands.length ? (
+                                            <ul className="brands_list">
+                                                {this.state.brands.map(
+                                                    (item, index) => {
+                                                        return (
+                                                            <li
+                                                                className="brand"
+                                                                key={index}
+                                                            >
+                                                                <a href="javascript:void(0)">
+                                                                    {item}
+                                                                </a>
+                                                            </li>
+                                                        );
+                                                    }
+                                                )}
+                                            </ul>
+                                        ) : (
+                                            'No Brands!'
+                                        )}
                                     </div>
-                                    {this.state.brands &&
-                                    this.state.brands.length ? (
-                                        <ul className="brands_list">
-                                            {this.state.brands.map(
-                                                (item, index) => {
-                                                    return (
-                                                        <li
-                                                            className="brand"
-                                                            key={index}
-                                                        >
-                                                            <a href="javascript:void(0)">
-                                                                {item}
-                                                            </a>
-                                                        </li>
-                                                    );
-                                                }
-                                            )}
-                                        </ul>
-                                    ) : (
-                                        'No Brands!'
-                                    )}
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="col-lg-9">
-                            <div className="shop_content">
-                                <div className="shop_bar clearfix">
-                                    <div className="shop_product_count">
-                                        <span>
-                                            {this.state.products.length}
-                                        </span>
-                                        Products found
+                            <div className="col-lg-9">
+                                <div className="shop_content">
+                                    <div className="shop_bar clearfix">
+                                        <div className="shop_product_count">
+                                            <span>
+                                                {this.state.products.length}
+                                            </span>
+                                            Products found
+                                        </div>
+                                        <div className="shop_sorting">
+                                            <span>Sort by:</span>
+                                            <ul>
+                                                <li>
+                                                    <span className="sorting_text">
+                                                        {
+                                                            this.state.sort
+                                                                .selected
+                                                        }
+                                                        <i className="fas fa-chevron-down" />
+                                                    </span>
+                                                    <ul>
+                                                        {this.state.sort.keys.map(
+                                                            (item, index) => {
+                                                                return (
+                                                                    <li
+                                                                        className="shop_sorting_button"
+                                                                        key={
+                                                                            index
+                                                                        }
+                                                                        onClick={() => {
+                                                                            this.sortProducts(
+                                                                                item
+                                                                            );
+                                                                        }}
+                                                                    >
+                                                                        {item}
+                                                                    </li>
+                                                                );
+                                                            }
+                                                        )}
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                    <div className="shop_sorting">
-                                        <span>Sort by:</span>
-                                        <ul>
-                                            <li>
-                                                <span className="sorting_text">
-                                                    highest rated
-                                                    <i className="fas fa-chevron-down" />
-                                                </span>
-                                                <ul>
-                                                    <li className="shop_sorting_button">
-                                                        highest rated
-                                                    </li>
-                                                    <li className="shop_sorting_button">
-                                                        name
-                                                    </li>
-                                                    <li className="shop_sorting_button">
-                                                        price
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
 
-                                <div className="product_grid row">
-                                    {this.state.products &&
-                                    this.state.products.length
-                                        ? this.state.products.map(item => {
-                                              return (
-                                                  <ShopItem
-                                                      productInfo={item}
-                                                      key={item.id}
-                                                  />
-                                              );
-                                          })
-                                        : 'No Products Available!'}
+                                    <div className="product_grid row">
+                                        {this.state.products &&
+                                        this.state.products.length
+                                            ? this.state.products.map(item => {
+                                                  return (
+                                                      <ShopItem
+                                                          productInfo={item}
+                                                          key={item.id}
+                                                      />
+                                                  );
+                                              })
+                                            : 'No Products Available!'}
+                                    </div>
                                 </div>
                             </div>
                         </div>
