0fa54c2ca25e034cd506a6e03ddde66d757123d4
Yogesh Rathod <yogesh.rathod@loylty.in>
2019-01-19 20:50:02 +0530

    API URL's of Node

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,18 +1,19 @@
-// const API_URL = {
-//     bestSeller: `https://ecommerce-application.herokuapp.com/products/getTop20`,
-//     banners: `https://ecommerce-application.herokuapp.com/products/getBanners`,
-//     recents: `https://ecommerce-application.herokuapp.com/products/getRecents`,
-//     categories: `https://ecommerce-application.herokuapp.com/products/getCategories`,
-//     loginUrl: `https://ecommerce-application.herokuapp.com/users/`,
-//     productsUrl: `https://ecommerce-application.herokuapp.com/products/cart`
-// };
-
 const API_URL = {
-    bestSeller: `https://api.myjson.com/bins/81wx8`,
-    banners: `https://api.myjson.com/bins/rff9g`,
-    recents: `https://api.myjson.com/bins/yags4`,
-    categories: `https://api.myjson.com/bins/md7uk`,
-    shopUrl: `https://api.myjson.com/bins/1axm1k`
+    bestSeller: `https://ecommerce-application.herokuapp.com/products/getTop20`,
+    banners: `https://ecommerce-application.herokuapp.com/products/getBanners`,
+    recents: `https://ecommerce-application.herokuapp.com/products/getRecents`,
+    categories: `https://ecommerce-application.herokuapp.com/products/getCategories`,
+    loginUrl: `https://ecommerce-application.herokuapp.com/users/`,
+    productsUrl: `https://ecommerce-application.herokuapp.com/products/cart`,
+    shopUrl: `https://ecommerce-application.herokuapp.com/products/getFilterProducts`
 };
+
+// const API_URL = {
+//     bestSeller: `https://api.myjson.com/bins/81wx8`,
+//     banners: `https://api.myjson.com/bins/rff9g`,
+//     recents: `https://api.myjson.com/bins/yags4`,
+//     categories: `https://api.myjson.com/bins/md7uk`,
+//     shopUrl: `https://api.myjson.com/bins/1axm1k`
+// };
 
 export default API_URL;
