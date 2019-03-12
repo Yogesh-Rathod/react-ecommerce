0f1d4d0e6fc3731490fbd2ff6853f4ab25ff6f4f
Yogesh Rathod <yogesh.rathod@loylty.in>
2019-03-12 12:29:51 +0530

    Different ENV files

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,29 +1,17 @@
+import * as ProdApi from './environment.prod.json';
+import * as DevApi from './environment.dev.json';
 
 let API_URL = {};
 
 switch (process.env.REACT_APP_ENV) {
     case 'prod':
-        API_URL = {
-            bestSeller: `https://ecommerce-application.herokuapp.com/products/getTop20`,
-            banners: `https://ecommerce-application.herokuapp.com/products/getBanners`,
-            recents: `https://ecommerce-application.herokuapp.com/products/getRecents`,
-            categories: `https://ecommerce-application.herokuapp.com/products/getCategories`,
-            loginUrl: `https://ecommerce-application.herokuapp.com/users/`,
-            productsUrl: `https://ecommerce-application.herokuapp.com/products/cart`,
-            shopUrl: `https://ecommerce-application.herokuapp.com/products/getFilterProducts`
-        };
+        API_URL = ProdApi.default;
         break;
     case 'dev':
-        API_URL = {
-            bestSeller: `https://api.myjson.com/bins/81wx8`,
-            banners: `https://api.myjson.com/bins/rff9g`,
-            recents: `https://api.myjson.com/bins/yags4`,
-            categories: `https://api.myjson.com/bins/md7uk`,
-            shopUrl: `https://api.myjson.com/bins/1axm1k`
-        };
+        API_URL = DevApi.default;
         break;
-
     default:
+        API_URL = DevApi.default;
         break;
 }
 
